51a2e4be788082b911ecdcc87d4cdd12d29e36a2
João Pedro Silva <90197643+joaopedro711@users.noreply.github.com>
2023-08-14 14:42:36 -0300

    interface python

---
+++ Interface/versao1/arapuka.py
@@ -0,0 +1,63 @@
+import PySimpleGUI as sg
+import eventos 
+
+app_title = 'Arapuka v1'
+themes = sorted(list(sg.LOOK_AND_FEEL_TABLE.keys()))
+sg.ChangeLookAndFeel('DarkBlack')
+sg.SetOptions(font='any 11', auto_size_buttons=True, progress_meter_border_depth=0, border_width=1)
+
+
+# gui design
+def create_window():
+    # main tab
+    layout = [[sg.T('', size=(15, 1)), sg.Text('Arapuka', font='Any 30')],
+              
+                [sg.Text('\n\nEstados:')],
+                # botões dos estados
+                [sg.Button('Dormente', size=(10, 1), pad=(10, 10)), sg.Button('Vigilia', size=(10, 1), pad=(10, 10)), sg.Button('Alerta 1', size=(10, 1), pad=(10, 10)), sg.Button('Alerta 2', size=(10, 1), pad=(10, 10))], 
+                [sg.Button('Suspeito', size=(10, 1), pad=(10, 10))],
+                
+                [sg.Text('\n\nAções:')],
+                #botoes das açoes extras
+                [sg.Button('Status', size=(10, 1), pad=(10, 20)), sg.Button('Rd n', size=(10, 1), pad=(10, 20)), sg.Button('Rd n_m', size=(10, 1), pad=(10, 20)), sg.Button('Apagar Memoria', size=(10, 1), pad=(10, 20))],
+                [sg.Button('E-mail', size=(10, 1), pad=(10, 20)), sg.Button('Resete', size=(10, 1), pad=(10, 20))],                   
+            ]
+
+
+
+    # window
+    return sg.Window(title=app_title, layout=layout, size=(500, 450), icon='arapuka.ico')
+
+window = create_window()
+
+while True:
+    event, values = window.read()
+    
+    if event == sg.WINDOW_CLOSED:
+        break
+
+    #Eventos dos Estados
+    elif event == 'Dormente':
+        eventos.dormente()
+    elif event == 'Vigilia':
+        eventos.vigilia()    
+    elif event == 'Alerta 1':
+        eventos.alerta_1()
+    elif event == 'Alerta 2':
+        eventos.alerta_2() 
+    elif event == 'Suspeito':
+        eventos.suspeito()
+
+    #Eventos das Ações
+    elif event == 'Status':
+        eventos.status()    
+    elif event == 'Rd n':
+        eventos.rd_n()
+    elif event == 'Rd n_m':
+        eventos.rd_n_m()     
+    elif event == 'Apagar Memoria':
+        eventos.apagar_memoria()    
+    elif event == 'E-mail':
+        eventos.email()
+    elif event == 'Resete':
+        eventos.resete()         
---
+++ Interface/versao1/eventos.py
@@ -0,0 +1,36 @@
+import PySimpleGUI as sg
+
+# Funções para cada botão dos Estados
+def dormente():
+    sg.popup('Botão 1 pressionado')
+
+def vigilia():
+    sg.popup('Botão 2 pressionado')
+
+def alerta_1():
+    sg.popup('Botão 3 pressionado')
+
+def alerta_2():
+    sg.popup('Botão 4 pressionado')
+
+def suspeito():
+    sg.popup('Botão 5 pressionado')
+
+# Funções para cada botão das Ações
+def status():
+    sg.popup('Botão 6 pressionado')
+
+def rd_n():
+    sg.popup('Botão 7 pressionado')
+
+def rd_n_m():
+    sg.popup('Botão 8 pressionado')
+
+def apagar_memoria():
+    sg.popup('Botão 9 pressionado')
+
+def email():
+    sg.popup('Botão 10 pressionado')
+
+def resete():
+    sg.popup('Botão 11 pressionado')
