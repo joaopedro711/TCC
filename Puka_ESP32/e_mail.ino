c3b89f1d36337c67e87b93b77a259cb2531ebb5e
João Pedro Silva <90197643+joaopedro711@users.noreply.github.com>
2023-08-23 14:48:51 -0300

    email and whatsapp

---
+++ Puka_ESP32/e_mail.ino
@@ -0,0 +1,74 @@
+bool e_mail(String mensagem, String e_mail_to){
+  // Configurações do servidor SMTP2GO
+  char* server = "mail.smtp2go.com";
+  int port = 465;
+  
+  // Configurações da conta de e-mail do servidor smtp2go
+  char* email = "arapuka@myyahoo.com";
+  char* email_password = "tcc_joaopedro_2023";
+  
+  // E-mail de envio(conforme configurado no smtp2go) e recepção(qualquer e-mail)
+  char* email_from = "arapukaunb@gmail.com";
+  String email_to = e_mail_to;
+
+  // Inicializar a conexão segura com o servidor SMTP
+  WiFiClientSecure client;
+  client.setInsecure(); // Usar essa opção somente se o servidor SMTP não tiver certificado SSL válido
+
+  if (!client.connect(server, port)) {
+    Serial.println("Falha na conexão com o servidor SMTP!");
+    return false;
+  }
+  Serial.println("Conectado ao servidor SMTP!");
+
+  // Esperar a resposta do servidor SMTP
+  waitForResponse(client);
+
+  // Enviar comandos SMTP para enviar o e-mail
+  client.println("EHLO 192.168.1.123");
+  waitForResponse(client);
+
+  client.println("AUTH LOGIN");
+  waitForResponse(client);
+
+  client.println(base64::encode(email));
+  waitForResponse(client);
+
+  client.println(base64::encode(email_password));
+  waitForResponse(client);
+
+  client.println("MAIL FROM:<" + String(email_from) + ">");
+  waitForResponse(client);
+
+  client.println("RCPT TO:<" + String(email_to) + ">");
+  waitForResponse(client);
+
+  client.println("DATA");
+  waitForResponse(client);
+
+  client.println("Subject: Arapuka móvel");
+  client.println("From: <" + String(email_from) + ">");
+  client.println("To: <" + String(email_to) + ">");
+  client.println("Content-Type: text/plain");
+  client.println(mensagem);
+  client.println(".");
+  waitForResponse(client);
+
+  client.println("QUIT");
+  waitForResponse(client);
+
+  // Fechar a conexão com o servidor SMTP
+  client.stop();
+  return true;
+}
+
+// Função para esperar a resposta do servidor SMTP com delays adequados
+void waitForResponse(WiFiClientSecure& client) {
+  while (!client.available()) {
+    delay(100);
+  }
+  while (client.available()) {
+    String response = client.readStringUntil('\n');
+    Serial.println(response);
+  }
+}
---
+++ Puka_ESP32/whatsapp.ino
@@ -0,0 +1,22 @@
+//Envia mensagem para o whatsApp (precisa inserir o numero e Key nas variaveis globais da main)
+void WhatsAppMessages(String message){
+    int i=0;
+    Serial.println("Iniciando Post");
+    // Dados a serem enviados pelo método Post suportado pelo protocolo HTTP
+    String url = "https://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);    
+    
+    HTTPClient http;                                                            // Cria um objeto HTTP
+    http.begin(url);                                                            // Inicia o objeto HTTP passando a string acima
+    
+    http.addHeader("Content-Type", "application/x-www-form-urlencoded");        // Adiciona um cabeçalho
+  
+    uint16_t httpResponseCode = http.POST(url);                                 // Envia o método POST HTTP de requisição e lê o resultado
+    Serial.println(url);
+    while(httpResponseCode != 200 && i<5){  
+        httpResponseCode = http.POST(url);
+        i++;
+        Serial.println("Deu ruim");
+    }
+    
+    http.end();                                                                 // Finaliza o objeto http
+}
